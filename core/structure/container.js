cb1b9e5aa463bb93bdec92eeb808ef62b420d0b9
fgambetta <fabiangambetta@gmail.com>
2025-02-01 21:12:22 -0300

    commit: Propiedades extra para item y container

--- core/structure/container.js
+++ core/structure/container.js
@@ -1,7 +1,12 @@
 class Container {
     _childs = [];
+    _width = 0;
+    _heigth = 0;
 
-    constructor(childs) {
-        this._childs;
+    constructor(childs, height, width) {
+        this._childs = childs;
+        this._heigth = height;
+        this._width = width;
+
     }
 }
--- core/structure/item.js
+++ core/structure/item.js
@@ -2,8 +2,13 @@ class Item {
 
     _isVisible = false;
     _node = null;
+    _index = undefined;
+    _width = 0;
+    _heigth = 0;
 
-    constructor() {}
+    constructor(index) {
+        this._index = index;
+    }
 
     set isVisible(value) {
         this._isVisible = value;
